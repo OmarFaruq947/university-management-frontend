87c40c13664e4ce360120acd72975a6f42180291
OmarFaruq947 <omarfaruq.bplus.6600@gmail.com>
2023-10-25 12:39:35 +0600

    not found page restructure

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,24 @@
+import Image from "next/image";
+import Link from "next/link";
+import notFoundImage from "../assets/404-Error-amico.png";
+
 const NotFoundPage = () => {
   return (
-    <div>
-      <h1> 404 ! page is note found😭</h1>
-    </div>
+    <>
+      <div className="wrapper">
+        <div className="landing-page">
+          <div className="">
+            <Image src={notFoundImage} width={200} alt="Not found..." />
+          </div>
+          <h1> 404 Error.</h1>
+          <p> We can't find the page you're looking for.</p>
+
+          <Link className="button" href={"/profile"}>
+            Back to home
+          </Link>
+        </div>
+      </div>
+    </>
   );
 };
 
